0bb4b904ab15ece47466fe9785e9c2772d86893d
IrvingM18 <irvingmoran507@gmail.com>
2023-12-11 00:46:44 -0500

    Agregar boton

--- Frontend/Net-SE/src/app/tab1/tab1.page.ts
+++ Frontend/Net-SE/src/app/tab1/tab1.page.ts
@@ -7,6 +7,9 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  iniciar() {
+    // Puedes agregar lógica aquí si es necesario
+    console.log('Botón "Iniciar" presionado');
 
+  }
 }
